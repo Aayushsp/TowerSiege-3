e1db630af9b4989c3e84d53df1ae106f4a0faf97
Aayushsp <72445567+Aayushsp@users.noreply.github.com>
2020-12-26 21:36:51 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,41 @@
+class Box{
+    constructor(x,y,width,height){
+        var options = {
+            isStatic: false
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+
+        this.width = width;
+        this.height = height;
+
+        World.add(world,this.body);
+
+        this.image = loadImage("Block.png");
+
+        this.visibility = 255;
+    }
+     score(){
+        if(this.visibility < 0 && this.visibility > -105){
+            score++;
+        }
+    }
+    display(){
+        var pos = this.body.position;
+
+        if(this.body.speed < 3){
+            imageMode(CENTER);
+            image(this.image, pos.x, pos.y, this.width, this.height);
+        }
+        else{
+            World.remove(world, this.body);
+            push()
+            this.visibility = this.visibility - 5;
+            tint(255,this.visibility);
+            imageMode(CENTER);
+            image(this.image, pos.x, pos.y, this.width, this.height);
+            pop();
+        }
+        
+    }
+   
+}
